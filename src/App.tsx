30f5fc5e2079a714fbf4f2978c691548ef7905e6
NeoAlchemy <NeoAlchemy@gmail.com>
2023-05-26 17:27:17 -0500

    Added masonry and church images

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import * as React from 'react';
-import Masonry from '@mui/lab/Masonry';
+import { Masonry } from 'react-plock';
+import { datasource } from './assets/datasource';
 import './style.css';
 
 export default function App() {
   return (
     <div>
       <center>
-        <h1>Hello StackBlitz!</h1>
+        <h1>Reflectify</h1>
       </center>
-      <Masonry columns={4} spacing={2}>
-        {heights.map((height, index) => (
-          <Item key={index} sx={{ height }}>
-            {index + 1}
-          </Item>
-        ))}
-      </Masonry>
+      <Masonry
+        items={datasource}
+        config={{
+          columns: [1, 2, 3],
+          gap: [24, 12, 6],
+          media: [640, 1024, 1280],
+        }}
+        render={(item, idx) => (
+          <img
+            key={idx}
+            src={item}
+            loading="lazy"
+            style={{ width: '100%', height: 'auto' }}
+          />
+        )}
+      />
     </div>
   );
 }
---
+++ src/assets/datasource.ts
@@ -0,0 +1,14 @@
+export const datasource = [
+  'https://assets.churchofjesuschrist.org/13/5b/135b0f8540681e6f940bdb7c203019762a44a65c/meme_moroni_challenge.jpeg',
+  'https://assets.churchofjesuschrist.org/26/5d/265db67e6514671d89404fd24f538fe925f7eff0/meme_mosiah.jpeg',
+  'https://assets.churchofjesuschrist.org/df/29/df2915a97b522b3965f69b1b1b753d38c4bb2eee/meme_enos_1.jpeg',
+  'https://assets.churchofjesuschrist.org/3e/f7/3ef7c5f3bfe79030d7052f765e81255feef8ebf5/quote_meme_alma.jpeg',
+  'https://assets.churchofjesuschrist.org/7d/28/7d28097fe0bbbfd5e376f859acd84a75292ba9e1/come_follow_me.jpeg',
+  'https://assets.churchofjesuschrist.org/8f/cd/8fcd09904573e04b9a498e2e2d28a3f42c63ae3f/meme_nephi_manner_men.jpeg',
+  'https://assets.churchofjesuschrist.org/fa/40/fa404d0a6ee4e4649abdad1111c31e4f33ae540a/meme_jacob_wise.jpeg',
+  'https://assets.churchofjesuschrist.org/ac/c9/acc9f4124181c7f3a7995e28d82b9fdb49152388/come_follow_me.jpeg',
+  'https://assets.churchofjesuschrist.org/41/00/4100c5e7e7f72c81c545ca19f9084f3364142236/meme_moroni_come_deny.jpeg',
+  'https://assets.churchofjesuschrist.org/14/9b/149bac3eb307652bcc54e7df8abb2ba11a083f75/meme_alma_counsel.jpeg',
+  'https://assets.churchofjesuschrist.org/11/06/11062b233c384c929a9076b614d6769494e9e9c8/come_follow_me.jpeg',
+  'https://assets.churchofjesuschrist.org/14/6d/146ddbafc7b96c4120d8263d3bfad62fd2c9595f/meme_nephi_go_do.jpeg',
+];
